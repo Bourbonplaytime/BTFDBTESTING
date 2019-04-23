7689ec4d0b873bca4019e3b0c358cb0fb867d74a
Bourbs <Bourbonplaytime@gmail.com>
2019-04-23 10:17:17 -0700

    authentication and session handling

---
+++ continue.php
@@ -0,0 +1,21 @@
+<?php // continue.php
+  session_start();
+
+  if (isset($_SESSION['forename']))
+  {
+    $forename = ($_SESSION['forename']);
+    $surname = ($_SESSION['surname']);
+
+    destroy_session_and_data();
+
+    echo "Welcome back $forename.<br> Your full name is $forename $surname. <br>";
+  }
+  else echo "Please <a href=authenticate2.php>click here</a> to log in.";
+
+  function destroy_session_and_data()
+  {
+    $_SESSION = array();
+    setcookie(session_name(), '', time() - 2592000, '/');
+    session_destroy();
+  }
+?>
